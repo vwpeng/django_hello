ab2eeeae77cba866c8e7ccf7a5eedda99be32604
vwpeng <vwpeng@gmail.com>
2020-03-02 23:57:04 +0800

    test

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponse
 
 
 # Create your views here.
-
+#
 def index(request):
     # print("hello world")
     return HttpResponse("hello django 用DJANGO创建的第一个应用")
