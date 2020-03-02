ab70d3280f735df227a751d9d1e26e4c9c29e058
vwpeng <vwpeng@gmail.com>
2020-03-02 23:48:12 +0800

    用django创建的第一个应用helloword

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    # print("hello world")
+    return HttpResponse("hello django 用DJANGO创建的第一个应用")
